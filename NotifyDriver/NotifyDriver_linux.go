dfc5c7c00b0c2d74737fb495fc63d4f70530475f
fenril22 <p.rest.k1.ke.se@gmail.com>
2018-05-19 18:59:33 +0900

    Fix Compile error

---
+++ NotifyDriver/NotifyDriver_linux.go
@@ -0,0 +1,47 @@
+// +build linux
+
+package notifydriver
+
+import (
+	"bufio"
+	"github.com/dbatbold/beep"
+	"github.com/gen2brain/beeep"
+	"log"
+	"runtime"
+	"strings"
+)
+
+func Notify(title string, body string, img_path string) {
+	LinuxNotify(title, body, img_path)
+}
+
+func BeeepNotify(title string, body string, img_path string) {
+	err := beeep.Notify(title, body, "")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func LinuxNotify(title string, body string, img_path string) {
+	music := beep.NewMusic("") // output can be file as "music.wav"
+	volume := 25
+
+	if err := beep.OpenSoundDevice("default"); err != nil {
+		log.Fatal(err)
+	}
+	if err := beep.InitSoundDevice(); err != nil {
+		log.Fatal(err)
+	}
+	beep.PrintSheet = false
+	defer beep.CloseSoundDevice()
+
+	musicScore := `
+		VP SA8 SR9 A9HRDE q
+    `
+
+	reader := bufio.NewReader(strings.NewReader(musicScore))
+	go music.Play(reader, volume)
+	BeeepNotify(title, body, img_path)
+	music.Wait()
+	beep.FlushSoundBuffer()
+}
